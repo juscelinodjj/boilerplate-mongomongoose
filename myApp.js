8dbd72c2a1b3bde374a1800dca371120dd51851a
Juscelino <juscelinodjj@gmail.com>
2021-09-30 15:18:08 -0300

    Add mongoose to myApp.js

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 
+const mongoose = require('mongoose');
+const mongoUri = process.env.MONGO_URI;
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
