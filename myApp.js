9ddef498356368af4eb7d29a3d5b70bcbd67e91b
Juscelino <juscelinodjj@gmail.com>
2021-09-30 17:23:21 -0300

    Add personSchema to myApp.js

--- myApp.js
+++ myApp.js
@@ -4,7 +4,15 @@ const mongoose = require('mongoose');
 const mongoUri = process.env.MONGO_URI;
 mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const { Schema } = mongoose;
+
+const personSchema = new Schema({
+  name:  { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+const Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
